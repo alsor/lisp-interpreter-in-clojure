cd7f80a3b424b832b76e152147bf10459b96ea76
Alexey Sorokin <alsor.net@gmail.com>
2017-11-14 00:06:03 +0300

    eval sequences of expressions

--- src/lisp_interpreter_in_clojure/core.clj
+++ src/lisp_interpreter_in_clojure/core.clj
@@ -1,10 +1,33 @@
 (ns lisp-interpreter-in-clojure.core)
 
-(defn my-eval [exp env]
-  ;(println (first exp))
-  (cond
-    (symbol? exp) (env exp)
-    (number? exp) exp
-    (= 'define (first exp)) (assoc env (second exp) (nth exp 2))
+(declare my-eval)
 
+(defn tagged-list? [exp tag]
+  (and (list? exp) (= (first exp) tag)))
+
+(defn self-evaluating? [exp]
+  (or (number? exp) (string? exp)))
+
+(defn variable? [exp]
+  (symbol? exp))
+
+(defn definition? [exp]
+  (tagged-list? exp 'define))
+
+(defn do? [exp]
+  (tagged-list? exp 'do))
+
+(defn eval-do [exps env]
+  (loop [rest-exps exps env env]
+    (if (= (count rest-exps) 1)
+      (my-eval (first rest-exps) env)
+      (do
+        (my-eval (first rest-exps) env)
+        (recur (rest rest-exps) env)))))
+
+(defn my-eval [exp env]
+  (cond
+    (self-evaluating? exp) exp
+    (variable? exp) (env exp)
+    (do? exp) (eval-do (rest exp) env)
     :else "syntax error"))
